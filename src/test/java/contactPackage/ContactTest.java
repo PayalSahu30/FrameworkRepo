09fc3e387bf6182459b7095686f70e16bb2ea28a
Hemant <Hemant@Hemant-Payal>
2025-01-10 10:21:11 +0530

    first commit

---
+++ src/test/java/contactPackage/ContactTest.java
@@ -0,0 +1,29 @@
+package contactPackage;
+import org.testng.annotations.Test;
+
+public class ContactTest 
+{
+@Test(groups="smokeTest")
+public void createContact()
+{
+	System.out.println("==executing CreateContact==");
+	
+	//Data from cmdLine
+	
+	String Browser=  System.getProperty("browser");
+	String URL=System.getProperty("url");
+	String USERNAME=System.getProperty("username");
+	String PASSWORD=System.getProperty("password");
+	
+	System.out.println(Browser);
+	System.out.println(URL);
+	System.out.println(USERNAME);
+	System.out.println(PASSWORD);
+}
+
+@Test(groups="regressionTest")
+public void modifyContact()
+{
+	System.out.println("==executing ModifyContact==");
+}
+}
---
+++ src/test/java/orgPackage/orgTest.java
@@ -0,0 +1,18 @@
+package orgPackage;
+
+import org.testng.annotations.Test;
+
+public class orgTest 
+{
+	@Test(groups="smokeTest")
+	public void createOrg()
+	{
+		System.out.println("==executing createOrg==");
+	}
+	
+	@Test(groups="regressionTest")
+	public void modifyOrg()
+	{
+		System.out.println("==executing ModifyOrg==");
+	}
+}
